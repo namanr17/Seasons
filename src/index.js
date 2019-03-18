05528f13848d163e0bee8364bc099d1f4659b8b4
x00-gh0st <naman17121998@gmail.com>
2019-03-19 00:48:13 +0530

    state

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = function() {
-  return <div>hello</div>;
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null, error: "" };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      error => {
+        this.setState({ error: error.message });
+      }
+    );
+  }
+
+  render() {
+    if (this.state.error && !this.state.lat) {
+      return <div>Error: {this.state.error}</div>;
+    }
+    if (this.state.lat && !this.state.erro) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+    return <div>loading..</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
